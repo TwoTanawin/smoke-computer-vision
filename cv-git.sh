de75b5f30b2dcfe749dec8f055c890b913f6bb95
TwoTanawin <tanawin.twox@gmail.com>
2023-11-24 02:53:32 +0700

    v4

--- cv-git.sh
+++ cv-git.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "v4"
-git pull origin main --allow-unrelated-histories
+git pull origin main 
 git push -u https://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@github.com/TwoTanawin/smoke-computer-vision.git main
